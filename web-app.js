97985f6c4017b76fa9f07f6d4d84ab7212b6ac78
RatWithCoffee <mashaglotova2003@gmail.com>
2024-03-23 01:17:36 +0300

    add test

--- web-app.js
+++ web-app.js
@@ -1,6 +1,22 @@
 const tg = window.Telegram.WebApp;
 
+const data = {
+    "name": "Тестовый опрос",
+    "questions": [
+        {
+            "question": "Как жизнь?",
+            "answers": ["Хорошо", "Плохо"]
+        },
+        {
+            "question": "Что интересного расскажешь?",
+            "answers": []
+        }
+    ]
+}
+
 const closeAppButton = document.getElementById('close_app');
 closeAppButton.addEventListener('click', () => {
+    tg.sendData(data);
+    console.log(data);
     tg.close();
 })
