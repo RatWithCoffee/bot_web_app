dffaa23a85404de44a564ee3dca2a7966460e636
RatWithCoffee <mashaglotova2003@gmail.com>
2024-03-23 14:25:57 +0300

    test

--- web-app.js
+++ web-app.js
@@ -1,22 +1,35 @@
 const tg = window.Telegram.WebApp;
 
-const data = {
-    "name": "Тестовый опрос",
-    "questions": [
-        {
-            "question": "Как жизнь?",
-            "answers": ["Хорошо", "Плохо"]
-        },
-        {
-            "question": "Что интересного расскажешь?",
-            "answers": []
-        }
-    ]
-}
 
-const closeAppButton = document.getElementById('close_app');
-closeAppButton.addEventListener('click', () => {
-    tg.sendData(JSON.stringify(data));
-    console.log(data);
+document.getElementById("survey").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    let name = document.getElementById("name").value;
+    let description = document.getElementById("description").value;
+    
+    let questions = [];
+
+    let questionDivs = document.querySelectorAll("[id^='question']");
+    questionDivs.forEach(function(questionDiv) {
+        let question = questionDiv.querySelector("input[type='text']").value;
+        let answers = [];
+        let answerInputs = questionDiv.querySelectorAll("input[id^='ans']");
+        answerInputs.forEach(function(answerInput) {
+            answers.push(answerInput.value);
+        });
+        questions.push({
+            question: question,
+            answers: answers
+        });
+    });
+
+    let surveyData = {
+        name: name,
+        description: description,
+        questions: questions
+    };
+
+    console.log(JSON.stringify(surveyData));
+    tg.sendData(JSON.stringify(surveyData));
     tg.close();
-})
+});
