1b11927f830354355a9a765872d50f6a94890eda
RatWithCoffee <mashaglotova2003@gmail.com>
2024-03-26 02:46:27 +0300

    save

--- js/web-app.js
+++ js/web-app.js
@@ -3,6 +3,13 @@ import { questionTypes } from "./components.js";
 
 const tg = window.Telegram.WebApp;
 
+var textarea = document.getElementById("try");
+var heightLimit = 200; /* Maximum height: 200px */
+
+textarea.oninput = function() {
+  textarea.style.height = ""; /* Reset the height*/
+  textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
+};
 // document.getElementById("add-question").addEventListener("click", () => {
 //     document.getElementById("type-selection-form").style.display = "block";
 // });
